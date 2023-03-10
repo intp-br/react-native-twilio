56e56c3b126b3ca7a19f6f685ee380221e1dd112
Lucas Vieira <lucasltv@gmail.com>
2023-03-10 13:39:28 -0300

    feat: twilio ios

--- ios/Twilio.m
+++ ios/Twilio.m
@@ -1,13 +1,18 @@
 #import <React/RCTBridgeModule.h>
+#import <React/RCTEventEmitter.h>
 
 @interface RCT_EXTERN_MODULE(Twilio, NSObject)
 
-RCT_EXTERN_METHOD(multiply:(float)a withB:(float)b
-                 withResolver:(RCTPromiseResolveBlock)resolve
-                 withRejecter:(RCTPromiseRejectBlock)reject)
+RCT_EXTERN_METHOD(isConnected: resolver:(RCTPromiseResolveBlock)resolve withRejecter:(RCTPromiseRejectBlock)reject)
+
+RCT_EXTERN_METHOD(startCall:(NSString *)accessToken withParams:(NSDictionary *)params)
+
+RCT_EXTERN_METHOD(endCall)
+RCT_EXTERN_METHOD(exitAppIosOnly)
 
 + (BOOL)requiresMainQueueSetup
 {
+  // https://stackoverflow.com/questions/50773748/difference-requiresmainqueuesetup-and-dispatch-get-main-queue
   return NO;
 }
 
--- ios/Twilio.swift
+++ ios/Twilio.swift
@@ -1,8 +1,151 @@
-@objc(Twilio)
-class Twilio: NSObject {
+import TwilioVoice
 
-  @objc(multiply:withB:withResolver:withRejecter:)
-  func multiply(a: Float, b: Float, resolve:RCTPromiseResolveBlock,reject:RCTPromiseRejectBlock) -> Void {
-    resolve(a*b)
-  }
+@objc(Twilio)
+class Twilio: RCTEventEmitter, CallDelegate {
+    var hasListeners = false
+    var audioDevice = DefaultAudioDevice()
+    var activeCall: Call? = nil
+
+    override func supportedEvents() -> [String]! {
+        return [
+            "CallConnected",
+            "CallConnectFailure",
+            "CallReconnecting",
+            "CallReconnected",
+            "CallDisconnected",
+            "CallDisconnectedError"
+        ]
+    }
+
+    override func startObserving() {
+        hasListeners = true
+    }
+
+    override func stopObserving() {
+        hasListeners = false
+    }
+
+    func callDidConnect(call: Call) {
+        NSLog("[TwilioPhone] callDidConnect")
+
+        if hasListeners {
+            sendEvent(withName: "CallConnected", body: ["callSid": call.sid])
+        }
+    }
+
+    func callDidFailToConnect(call: Call, error: Error) {
+        NSLog("[TwilioPhone] Call failed to connect: \(error.localizedDescription)")
+
+        if hasListeners {
+            sendEvent(withName: "CallConnectFailure", body: [
+                "callSid": call.sid,
+                "errorMessage": error.localizedDescription
+            ])
+        }
+    }
+
+    func callIsReconnecting(call: Call, error: Error) {
+        NSLog("[TwilioPhone] Call is reconnecting with error: \(error.localizedDescription)")
+
+        if hasListeners {
+            sendEvent(withName: "CallReconnecting", body: [
+                "callSid": call.sid,
+                "errorMessage": error.localizedDescription
+            ])
+        }
+    }
+
+    func callDidReconnect(call: Call) {
+        NSLog("[TwilioPhone] Call did reconnect")
+
+        if hasListeners {
+            sendEvent(withName: "CallReconnected", body: ["callSid": call.sid])
+        }
+    }
+
+    func callDidDisconnect(call: Call, error: Error?) {
+        if let error = error {
+            NSLog("[TwilioPhone] Call disconnected with error: \(error.localizedDescription)")
+
+            if hasListeners {
+                sendEvent(withName: "CallDisconnectedError", body: [
+                    "callSid": call.sid,
+                    "errorMessage": error.localizedDescription
+                ])
+            }
+        } else {
+            NSLog("[TwilioPhone] Call disconnected")
+
+            if hasListeners {
+                sendEvent(withName: "CallDisconnected", body: ["callSid": call.sid])
+            }
+        }
+
+        activeCall = nil
+    }
+
+    @objc(startCall:withParams:)
+    func startCall(accessToken: String, params: [String: String]) -> Void {
+        NSLog("[TwilioPhone] Starting call")
+        let connectOptions = ConnectOptions(accessToken: accessToken) { builder in
+            builder.params = params
+        }
+
+        let call = TwilioVoiceSDK.connect(options: connectOptions, delegate: self)
+        activeCall = call
+    }
+
+    @objc(endCall)
+    func endCall() -> Void {
+        NSLog("[TwilioPhone] endCall")
+
+        if (activeCall === nil) {
+            return
+        }
+
+        activeCall?.disconnect()
+        activeCall = nil
+    }
+
+    @objc(isConnected:withRejecter:)
+    func isConnected(resolve: RCTPromiseResolveBlock, reject:RCTPromiseRejectBlock) -> Void {
+        NSLog("[TwilioPhone] callIsConnected")
+
+        if (activeCall === nil) {
+            resolve(false)
+            return
+        }
+
+        resolve(true)
+    }
+
+    @objc(exitAppIosOnly)
+    func exitAppIosOnly() -> Void {
+        NSLog("[TwilioPhone] exitAppIosOnly")
+
+        exit(0)
+    }
+
+}
+
+// TVOStatsReport JSON serialization
+extension StatsReport {
+    public func toJSON() -> NSDictionary {
+        return [
+            "localAudioTrackStats": localAudioTrackStats.map({ trackStats in
+                return [
+                    "audioLevel": trackStats.audioLevel,
+                    "jitter": trackStats.jitter,
+                    "roundTripTime": trackStats.roundTripTime,
+                ]
+            }),
+            "remoteAudioTrackStats": remoteAudioTrackStats.map({ trackStats in
+                return [
+                    "audioLevel": trackStats.audioLevel,
+                    "jitter": trackStats.jitter,
+                    "mos": trackStats.mos
+                ]
+            }),
+        ]
+    }
 }
